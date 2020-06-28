321bf0ae88cb4c1ae9601671c04c0b7d15047f27
Super <1125699801@qq.com>
2020-06-28 15:04:29 +0800

    更改显示时间格式

--- sayhello/forms.py
+++ sayhello/forms.py
@@ -11,6 +11,6 @@ from wtforms.validators import DataRequired, Length
 
 
 class HelloForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(1, 20)])
-    body = TextAreaField('Message', validators=[DataRequired(), Length(1, 200)])
-    submit = SubmitField()
+    name = StringField('姓名', validators=[DataRequired(), Length(1, 20)])
+    body = TextAreaField('消息', validators=[DataRequired(), Length(1, 200)])
+    submit = SubmitField('提交')
