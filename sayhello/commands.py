51024bb8c5c8148eda7aba456ebcaff539c25390
Super <1125699801@qq.com>
2020-06-28 14:45:15 +0800

    update author

--- sayhello/commands.py
+++ sayhello/commands.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-    :author: Grey Li (李辉)
-    :url: http://greyli.com
-    :copyright: © 2018 Grey Li <withlihui@gmail.com>
+    :author: Super (苏勇智)
+    :url:
+    :copyright: © 2020 Super
     :license: MIT, see LICENSE for more details.
 """
 import click
